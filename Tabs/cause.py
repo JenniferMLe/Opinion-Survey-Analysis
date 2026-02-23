346db537ef223424f3e27f207446565225b24065
Jennifer Le <122183026+JenniferMLe@users.noreply.github.com>
2026-02-22 22:05:28 -0600

    finalized hypothesis testing and causal inference tab

--- Tabs/cause.py
+++ Tabs/cause.py
@@ -8,38 +8,42 @@ demographics, social_media = utils.demographics, utils.social_media
 col, category_orders, colors = utils.col, utils.category_orders, utils.colors
 
 def show():
-    st.header("What cause the problem?")    
+    st.header("Did events in 2022 increase negative sentiment?")    
     st.markdown('<div class="border">'  
     "<b>Events happening in 2021 and 2022 could have affected sentiment</b> "
-    "such as Joe Biden taking office and rising inflation rates.<br><br> "
+    "such as Joe Biden taking office and rising inflation rates. "
 
     "<b>Inflation rates</b> represent the increase in prices compared to the previous year. " 
     "At its peak, the 12-month inflation rate was 9.1% in June of 2022 meaning prices on average " 
     "were 9.1% more expensive compared to June of 2021. View inflation rate trends from 2000-current "
-    '<a href="https://www.usinflationcalculator.com/inflation/current-inflation-rates/" target="_blank">here. </a><br><br>'
-
-    "Let's see if the increase in negative sentiment was caused by economic events in 2022 such as rising " 
-    "inflation rates by holding features than may affect it constant such as political affliation and age."
+    '<a href="https://www.usinflationcalculator.com/inflation/current-inflation-rates/" target="_blank">here. </a>'
     '</div>', unsafe_allow_html=True)
 
     st.markdown(
-        "\nUsing **causal inference**, doubly-robust correction, and the following set-up ... \n"  
+        "Defining the **hypothesis**\n"
+        "> - **Null Hypothesis** → 2022 events did not affect opinions. Average Treatment Effect (ATE) = 0\n"
+        "> - **Alternative Hypothesis** → 2022 events affected opinions. Average Treament Effect (ATE) ≠ 0\n"
+        "> - **Significance level** → 0.05\n"
+        "\n>**ATE** tells us how much the outcome would change if everyone experienced 2022 events compared to if " 
+        "no one experienced 2022 events. "
+        ,unsafe_allow_html=True
+    )
+
+    st.markdown(
+        "\nUsing **causal inference**, EconML's doubly-robust learner, and the following ... \n"  
         "> - **Control** → Year = 2021\n"
         "> - **Treatment** → Year = 2022\n"
         "> - **Outcome** → Overall Sentiment = 'Negative'\n"
-        "> - **Covariates** → variables that appear in the 2021 and 2022 datasets\n\n"
-
-        "We get an **average treatment effect** (ATE) of **0.21**. "
-        "This means if everyone was from 2022, the probability of having negative "
-        "sentiment would increase on average by 21 percentage points compared to if everyone was from 2021.\n\n"
-
-        "**Covariates** are uncontrolled variables that may affect the outcome. "
-        "For example if more republicans, religious people, or older adults happen to be selected for the 2022 study "
-        "(when Joe Biden was president), we can't reliably say econmic events of 2022 "
-        "caused the increase in negative sentiment because of selection bias.\n\n"
+        "> - **Covariates** → variables that appear in the 2021 and 2022 datasets\n"
+        "\n>**Covariates** are uncontrolled variables that may affect the outcome such as political party and religion.\n\n"
         
-        "Using double robust corrections, we can account for possible selection biases. ",
-        unsafe_allow_html=True
+        "We get the following **results**\n"
+        ">- **ATE** = 0.20\n"
+        ">- **p-value** = 0.64\n\n"
+
+        "Because p is greater than 0.05, **we don't have enough evidence to say events in 2022 affected opinions** " \
+        "or that the null hypothesis is false. Therefore we fail to reject the null hypothesis. "
+        ,unsafe_allow_html=True
     )
    
     
